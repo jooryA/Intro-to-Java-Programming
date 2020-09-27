06526b14eadf7b4a438c49dd9e7b35dbd0195f8a
jooryA <71961165+jooryA@users.noreply.github.com>
2020-09-27 21:27:00 +0300

    Update Exercise_01_08.java

--- Exercise_01/Exercise_01_08/Exercise_01_08.java
+++ Exercise_01/Exercise_01_08/Exercise_01_08.java
@@ -6,9 +6,10 @@ of a circle that has a radius of 5.5 using the following formula:
 */
 public class Exercise_01_08 {
 	public static void main(String[] args) {
-		System.out.println("Perimeter = ");
-		System.out.println(2 * 5.5 * 3.14159);
-		System.out.println("Area = ");
-		System.out.println(5.5 * 5.5 * 3.14159);
+		System.out.println("9.5 X 4.5 - 2.5 X 3");
+		System.out.println("-------------------");
+		System.out.println("   45.5 - 3.5 ");
+		System.out.println("        =");
+                System.out.println(((9.5 * 4.5 ) - (2.5 * 3)) / (45. - 3.5);
 	}
 }
